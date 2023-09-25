222b82af81c380c2569e9b9d4d50d9606555e0ea
fabriciosiqueira08 <fabricio.siqueira@aluno.ufop.edu.br>
2023-09-25 15:22:31 -0300

    Aprimorado o componente button

--- calculadora/src/components/Button.jsx
+++ calculadora/src/components/Button.jsx
@@ -2,7 +2,19 @@ import React from "react";
 import "./Button.css";
 
 const Button = (props) => {
-  return <button className="button">{props.label}</button>;
+  return (
+    <button
+      onClick={(e) => props.click && props.click(props.label)}
+      className={`
+    button
+    ${props.operation ? "operation" : ""}
+    ${props.double ? "double" : ""}
+    ${props.triple ? "triple" : ""}
+  `}
+    >
+      {props.label}
+    </button>
+  );
 };
 
 export default Button;
